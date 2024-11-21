cdf04e5b807d7f7c384c25a4ecb1597de86e53ab
UTakuto <taku.0307.ue@icloud.com>
2024-11-22 00:12:43 +0900

    first commit

---
+++ src/app/camera/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function page() {
+    return (
+        <div>
+            <h1>カメラ</h1>
+        </div>
+    );
+}
---
+++ src/app/pictureBook/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function page() {
+    return (
+        <div>
+            <h1>図鑑画面</h1>
+        </div>
+    );
+}

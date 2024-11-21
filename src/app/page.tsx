1d1455dd88e6068b03ee1c54e0eb585139a403e1
UTakuto <taku.0307.ue@icloud.com>
2024-11-22 00:37:54 +0900

    カメラ機能を追加し、写真撮影のためのコンポーネントを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Link from "next/link";
+import Camera from "./camera/page";
 
 export default function page() {
     return (
         <div>
-            <Link href="./camera">camera</Link>
+            <Camera />
+            {/* <Link href="./camera">camera</Link> */}
             <Link href="./pictureBook">picture book</Link>
         </div>
     );

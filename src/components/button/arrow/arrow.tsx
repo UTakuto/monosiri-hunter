1b87bd0da655705cf7a1033352d426ecb05ca3f2
UTakuto <taku.0307.ue@icloud.com>
2024-12-26 16:51:09 +0900

    style変更。link更新

--- src/components/button/arrow/arrow.tsx
+++ src/components/button/arrow/arrow.tsx
@@ -1,13 +1,25 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import Image from "next/image";
 import "./arrow.css";
 
 export default function Arrow() {
     const router = useRouter();
 
+    const pathname = usePathname();
+
+    const handleBack = () => {
+        const currentPath = pathname;
+        if (currentPath) {
+            const parentPath = currentPath.substring(0, currentPath.lastIndexOf("/")) || "/";
+            router.push(parentPath);
+        } else {
+            console.log("現在のパスが取得できません");
+        }
+    };
+
     return (
-        <div className="backBtnWrap" onClick={() => router.back()}>
+        <div className="backBtnWrap" onClick={handleBack}>
             <div className="backBtn">
                 <div className="image">
                     <Image src="/arrow.png" alt="戻る" width={50} height={50} />

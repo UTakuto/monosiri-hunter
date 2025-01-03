68933c32ecbff2acf2659d996315f183883dfcb9
UTakuto <taku.0307.ue@icloud.com>
2025-01-04 03:06:54 +0900

    キャラクター画像を追加し、ロゴのスタイルを更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,21 @@
 import Camera from "../components/button/camera/camera";
 import Picture from "../components/button/picture/Picture";
 import style from "./index.module.css";
+import Image from "next/image";
 
 export default function Page() {
     return (
         <div className={style.container}>
             <div className={style.wrapper}>
-                <div className={style.logo}></div>
+                <div className={style.logo}>
+                    <Image
+                        className={style.character}
+                        src="/character.png"
+                        alt="キャラクター"
+                        width={150}
+                        height={150}
+                    />
+                </div>
                 <div className={style.btnWrap}>
                     <Camera />
                     <Picture />

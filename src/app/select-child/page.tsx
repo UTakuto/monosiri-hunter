d7eaeb3f93004f2aa2fe84a0c7a0daf234ae0670
UTakuto <taku.0307.ue@icloud.com>
2025-02-16 03:26:39 +0900

    SelectChildコンポーネントのスタイルを改善し、CSSモジュールを適用

--- src/app/select-child/page.tsx
+++ src/app/select-child/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { RequireAuth } from "@/components/auth/RequireAuth";
 import { ChildList } from "@/components/child/ChildList";
+import style from "@/app/login/login.module.css";
 
 export default function SelectChild() {
     return (
         <RequireAuth>
-            <div className="container mx-auto p-4">
-                <h1 className="text-2xl font-bold mb-4">お子様を選択してください</h1>
+            <div className={style.selectContainer}>
+                <h1 className={style.title}>お子様を選択してください</h1>
                 <ChildList />
             </div>
         </RequireAuth>
